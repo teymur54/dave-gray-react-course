d4d0b3ef345949b7683406e15ca8bce47a50845c
teymur <teymurhasanov100@gmail.com>
2023-10-23 09:38:09 +0400

    message

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,77 @@ import Footer from './Footer';
 import Header from './Header';
 import SearchItem from './SearchItem';
 import AddItem from './AddItem';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import apiRequest from './apiRequest';
 
 function App() {
-  const [ items, setItems ] = useState(JSON.parse(localStorage.getItem('shoppingList')));
+  const API_URL = 'http://localhost:3500/items';
+
+  const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState('');
   const [search,setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem('shoppingList', JSON.stringify(newItems));  
-  }
+  useEffect(() => {
+   const fetchItems = async () => {
+    try{
+      const response = await fetch(API_URL);
+      if(!response.ok) throw Error('Did not receive expected data');
+      const listItems = await response.json();
+      setItems(listItems);
+      setFetchError(null)
+    } catch (err) {
+      setFetchError(err.message)
+    } finally{
+      setIsLoading(false)
+    }
+   }
+    fetchItems();
+  }, [])
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
+
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+    const result = await apiRequest(API_URL, postOptions);
+    if(result) setFetchError(result);
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item,checked: !item.checked} : item);
-    setAndSaveItems(listItems);
+    setItems(listItems);
+
+    const myİtem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ checked: myİtem[0].checked})
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if(result) setFetchError(result);
   }
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
+
+    const deleteOptions = { method: 'DELETE' };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if(result) setFetchError(result);
   }
 
   const handleSubmit = (e) => {
@@ -42,20 +86,24 @@ function App() {
   return (
     <>
       <Header title="Groceries List" />
-      <SearchItem
-        search={search}
-        setSearch={setSearch}
-      />
       <AddItem 
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
-      <Content 
-        items = {items}
-        handleCheck = {handleCheck}
-        handleDelete = {handleDelete}
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
       />
+      <main>
+        {isLoading && <p>Loading items...</p>}
+        {fetchError && <p style={{ color: "red"}}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && <Content 
+          items = {items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck = {handleCheck}
+          handleDelete = {handleDelete}
+        />}
+      </main>
       <Footer length={items.length} />
     </>
   );
--- src/SearchItem.jsx
+++ src/SearchItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SearchItem = () => {
+const SearchItem = ({ search, setSearch }) => {
   return (
     <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
       <label htmlFor='search'>Search</label>
@@ -9,6 +9,9 @@ const SearchItem = () => {
         type='text'
         role='searchbox'
         placeholder='Search Items'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        autoComplete='off'
       />
     </form>
   )
